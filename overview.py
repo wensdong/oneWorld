e59374c2a589dae3de19b968fa349b20f4ff5f37
we <wensdong@gmail.com>
2018-08-02 15:12:07 +0100

    add es

--- overview.py
+++ overview.py
@@ -3,14 +3,23 @@ overview
 def rollout(controller):
 ’’’ env, rnn, vae are ’’’
 ’’’ global variables ’’’
+
 obs = env.reset()
 h = rnn.initial_state()
 done = False
 cumulative_reward = 0
+
 while not done:
-z = vae.encode(obs)
-a = controller.action([z, h])
-obs, reward, done = env.step(a)
-cumulative_reward += reward
-h = rnn.forward([a, z, h])
+	z = vae.encode(obs)
+	a = controller.action([z, h])
+	obs, reward, done = env.step(a)
+	cumulative_reward += reward
+	h = rnn.forward([a, z, h])
 return cumulative_reward
+
+
+#abstract
+
+es = cma(w0,sgma0, popsize)
+f = rollout(controller)
+
